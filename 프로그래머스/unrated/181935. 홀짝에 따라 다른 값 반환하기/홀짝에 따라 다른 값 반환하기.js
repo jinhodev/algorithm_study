ee3c7b7da5f0b29af43abe08f609ca383a1ea1c1
Jinho Park <79356772+Emocloud@users.noreply.github.com>
2024-01-08 19:16:57 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let sum = 0
+    if(n%2==0){
+        for(let i = 2; i<=n; i+=2){
+            sum = sum + i*i
+        }
+    }else{
+        for(let i = 1; i<=n;i+=2){
+            sum = sum + i
+        }
+    }
+    var answer = sum;
+    return answer;
+}
